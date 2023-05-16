51041050c91efdebd7828c179cd1b9924b74cc73
Duoslow <16846504+Duoslow@users.noreply.github.com>
2023-05-16 08:28:22 +0300

    Nothing playing status

--- main.py
+++ main.py
@@ -9,16 +9,21 @@ arduino = serial.Serial(port=config['Arduino']['port'], baudrate=config['Arduino
 last_song = ''
 while True:
     current_track = spotify.current_user_playing_track()
-    artists = ', '.join([i['name'] for i in current_track['item']['artists']])
-    album = current_track['item']['album']['name']
-    track = current_track['item']['name']
-    now_playing = ' {} => {} '.format(artists, track)
-    if now_playing != last_song:
-        last_song = now_playing
-        # we need to close and reopen the serial connection to clear the lcd buffer
-        arduino.close() # Close serial connection to Arduino
-        arduino = serial.Serial(port=config['Arduino']['port'], baudrate=config['Arduino']['baudRate'], timeout=.1) # Reopen serial connection to Arduino
-    arduino.write(bytes(now_playing, 'utf-8'))
-    time.sleep(3)
-    # print('Currently playing: {}'.format(now_playing))
+    if current_track:
+        artists = ', '.join([i['name'] for i in current_track['item']['artists']])
+        album = current_track['item']['album']['name']
+        track = current_track['item']['name']
+        now_playing = ' {} => {} '.format(artists, track)
+        if now_playing != last_song:
+            last_song = now_playing
+            # we need to close and reopen the serial connection to clear the lcd buffer
+            arduino.close() # Close serial connection to Arduino
+            arduino = serial.Serial(port=config['Arduino']['port'], baudrate=config['Arduino']['baudRate'], timeout=.1) # Reopen serial connection to Arduino
+        arduino.write(bytes(now_playing, 'utf-8'))
+        print('Currently playing: {}'.format(now_playing))
+        time.sleep(3)
+    else:
+        arduino.write(bytes('Nothing playing', 'utf-8'))
+        print('Nothing playing')
+        time.sleep(3)
     
